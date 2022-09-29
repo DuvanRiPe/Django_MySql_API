995ea3a9a88b6e1f5f7a659d73781078a1a23232
DuvanRiPe <Duvanriverape96@gmail.com>
2022-09-29 11:18:35 -0500

    commit

--- api/views.py
+++ api/views.py
@@ -39,7 +39,18 @@ class CompanyView(View):
         Company.objects.create(name=jd['name'],website=jd['website'], foundation=jd['foundation'])
         datos={"message":"Success"}
         return JsonResponse(datos)
-    def put(self, request):
-        pass
+    def put(self, request, id):
+        jd=json.loads(request.body)
+        companies=list(Company.objects.filter(id=id).values())
+        if len(companies)>0:
+            company= Company.objects.get(id=id)
+            company.name=jd['name']
+            company.website=jd['website']
+            company.foundation=jd['foundation']
+            company.save()
+            datos={"message":"Success"}
+        else:
+            datos={"message":"Companies not found..."}
+        return JsonResponse(datos)
     def delete(self, request):
         pass
